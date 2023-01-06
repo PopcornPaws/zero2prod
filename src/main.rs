adddcead896c380c4b54c0118c5962324ea3c933
PopcornPaws <lipschitz77@gmail.com>
2023-01-06 16:00:03 +0100

    Small changes.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use sqlx::{Connection, PgConnection};
+use sqlx::PgPool;
 use zero2prod::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("failed to read configuration");
-    let connection = PgConnection::connect(&configuration.database.connection_string())
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("failed to connect to postgres");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = std::net::TcpListener::bind(&address).expect("failed to bind random port");
-    zero2prod::run(listener, connection)?.await
+    zero2prod::run(listener, connection_pool)?.await
 }
